3c3adc32c24252cd3a63b1a7b53175c5c629f4bf
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-04-27 14:24:48 -0400

    Created a new instance of handle bars

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const exphbs = require('express-handlebars');
 //initializes handlebars instance 
 const hbs = exphbs.create({});
 
-//Setting a handle ars as default templat engine for server. 
+//Setting a handle bars as default templat engine for server. 
 //Method used to set up template engine for rendering dynamic views in web application. First parameter is the file extension i.e .handlebars and 
-//second parameter tells Express to use engine function of hbs object to render rtemplat with handlebar extension
+//second parameter tells Express to use engine function of hbs object to render template with handlebar extension
 app.engine('handlebars', hbs.engine);
-//Setting view engine of express application to use handlebars
+//App Setting the view engine of express application to use handlebars
 app.set('view engine', 'handlebars');
